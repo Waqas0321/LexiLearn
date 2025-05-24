b21c311b3e2886762cd711a76d9025f50bd024cd
Waqas Akhtar <waqasakhtar548@gmail.com>
2025-05-24 17:30:27 +0500

    Alphabets Images and extension update

--- lib/faetures/ai/view/ai_screen.dart
+++ lib/faetures/ai/view/ai_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:lexi_learn/core/Const/app_images.dart';
 import 'package:lexi_learn/core/widgets/custom_appbar.dart';
 import '../../../core/Const/app_colors.dart';
 import '../../../core/helpers/date_formatter.dart';
@@ -24,138 +25,175 @@ class AiChatScreen extends StatelessWidget {
       builder: (context, constraints) {
         return Scaffold(
           backgroundColor: AppColors.white,
-          appBar: CustomAppBar(title: "Chat with Ai",goBack: true,),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Obx(
-                  () => controller.messages.isNotEmpty
-                      ? ListView.builder(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 2, horizontal: 6),
-                          itemCount: controller.messages.length,
-                          itemBuilder: (context, index) {
-                            final message = controller.messages[index];
-                            final isMe = message.isMe;
-                            final time =
-                                DateFormatter.formatDate(message.timestamp);
-                            return GestureDetector(
-                              onLongPress: () {
-                                Clipboard.setData(
-                                        ClipboardData(text: message.text))
-                                    .then(
-                                  (value) => toast.showCustomToast(
-                                      "Text copied successfully!"),
+          appBar: CustomAppBar(title: "Chat with Ai", goBack: true),
+          body: Container(
+            width: appSizes.getWidthPercentage(100),
+            height: appSizes.getHeightPercentage(100),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.background),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Obx(
+                    () =>
+                        controller.messages.isNotEmpty
+                            ? ListView.builder(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 2,
+                                horizontal: 6,
+                              ),
+                              itemCount: controller.messages.length,
+                              itemBuilder: (context, index) {
+                                final message = controller.messages[index];
+                                final isMe = message.isMe;
+                                final time = DateFormatter.formatDate(
+                                  message.timestamp,
+                                );
+                                return GestureDetector(
+                                  onLongPress: () {
+                                    Clipboard.setData(
+                                      ClipboardData(text: message.text),
+                                    ).then(
+                                      (value) => toast.showCustomToast(
+                                        "Text copied successfully!",
+                                      ),
+                                    );
+                                  },
+                                  child: Align(
+                                    alignment:
+                                        isMe
+                                            ? Alignment.centerRight
+                                            : Alignment.centerLeft,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          isMe
+                                              ? CrossAxisAlignment.end
+                                              : CrossAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          margin: EdgeInsets.only(
+                                            left:
+                                                isMe
+                                                    ? appSizes
+                                                        .getWidthPercentage(10)
+                                                    : 6,
+                                            right:
+                                                !isMe
+                                                    ? appSizes
+                                                        .getWidthPercentage(10)
+                                                    : 6,
+                                            top: 4,
+                                            bottom: 4,
+                                          ),
+                                          padding: const EdgeInsets.all(12),
+                                          decoration: BoxDecoration(
+                                            color:
+                                                isMe
+                                                    ? AppColors.orange
+                                                    : AppColors.orange2,
+                                            borderRadius: BorderRadius.only(
+                                              bottomRight:
+                                                  const Radius.circular(14),
+                                              bottomLeft: const Radius.circular(
+                                                16,
+                                              ),
+                                              topRight:
+                                                  isMe
+                                                      ? const Radius.circular(0)
+                                                      : const Radius.circular(
+                                                        16,
+                                                      ),
+                                              topLeft:
+                                                  isMe
+                                                      ? const Radius.circular(
+                                                        16,
+                                                      )
+                                                      : const Radius.circular(
+                                                        0,
+                                                      ),
+                                            ),
+                                          ),
+                                          child: CustomTextWidget(
+                                            text: message.text,
+                                            textColor:
+                                                isMe
+                                                    ? AppColors.white
+                                                    : AppColors.black,
+                                            fontSize: 14,
+                                            textOverflow: TextOverflow.visible,
+                                            textAlign: TextAlign.start,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                            left: 12,
+                                            right: 12,
+                                          ),
+                                          child: CustomTextWidget(
+                                            text: time,
+                                            fontSize: 11,
+                                            fontWeight: FontWeight.w400,
+                                            textColor: AppColors.whitish,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 );
                               },
-                              child: Align(
-                                alignment: isMe
-                                    ? Alignment.centerRight
-                                    : Alignment.centerLeft,
-                                child: Column(
-                                  crossAxisAlignment: isMe
-                                      ? CrossAxisAlignment.end
-                                      : CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.only(
-                                        left: isMe
-                                            ? appSizes.getWidthPercentage(10)
-                                            : 6,
-                                        right: !isMe
-                                            ? appSizes.getWidthPercentage(10)
-                                            : 6,
-                                        top: 4,
-                                        bottom: 4,
-                                      ),
-                                      padding: const EdgeInsets.all(12),
-                                      decoration: BoxDecoration(
-                                        color: isMe
-                                            ? AppColors.orange
-                                            : AppColors.orange2,
-                                        borderRadius: BorderRadius.only(
-                                          bottomRight:
-                                              const Radius.circular(14),
-                                          bottomLeft:
-                                              const Radius.circular(16),
-                                          topRight: isMe
-                                              ? const Radius.circular(0)
-                                              : const Radius.circular(16),
-                                          topLeft: isMe
-                                              ? const Radius.circular(16)
-                                              : const Radius.circular(0),
-                                        ),
-                                      ),
-                                      child: CustomTextWidget(
-                                        text: message.text,
-                                        textColor: isMe
-                                            ? AppColors.white
-                                            : AppColors.black,
-                                        fontSize: 14,
-                                        textOverflow: TextOverflow.visible,
-                                        textAlign: TextAlign.start,
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 12, right: 12),
-                                      child: CustomTextWidget(
-                                        text: time,
-                                        fontSize: 11,
-                                        fontWeight: FontWeight.w400,
-                                        textColor: AppColors.whitish,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        )
-                      : Center(
-                    child: CustomTextWidget(
-                      text: "Ai Chat not found",
+                            )
+                            : Center(
+                              child: CustomTextWidget(
+                                text: "Ai Chat not found",
 
-                      textColor: AppColors.whitish2,
-                    ),
+                                textColor: AppColors.whitish2,
+                              ),
+                            ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: CustomInputTextField(
-                        hintText: 'Type message...',
-                        borderColor: AppColors.orange,
-                        textEditingController: controller.messageController, labelText: 'Message',
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CustomInputTextField(
+                          hintText: 'Type message...',
+                          borderColor: AppColors.white,
+                          isFilled: true,
+                          textEditingController: controller.messageController,
+                          labelText: '',
+                        ),
                       ),
-                    ),
-                    const Gap(8),
-                    ElevatedButton(
-                      onPressed: () {
-                        controller
-                            .sendMessage(controller.messageController.text);
-                        controller.messageController.clear();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(10),
-                        backgroundColor: AppColors.orange,
+                      const Gap(8),
+                      ElevatedButton(
+                        onPressed: () {
+                          controller.sendMessage(
+                            controller.messageController.text,
+                          );
+                          controller.messageController.clear();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(10),
+                          backgroundColor: AppColors.white,
+                        ),
+                        child: const Icon(
+                          Icons.send,
+                          color: AppColors.orange,
+                          size: 29,
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.send,
-                        color: AppColors.white,
-                        size: 29,
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
